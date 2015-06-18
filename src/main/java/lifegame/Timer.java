073508d3793cb6e83a3182d4324ca5debe07ea2e
ahongdaong88 <jicholkim@zuminternet.com>
2015-06-18 09:39:15 +0900

    #1 타이머 구현... 이제 제대로 커밋하는듯합니다..

--- src/main/java/lifegame/Timer.java
+++ src/main/java/lifegame/Timer.java
@@ -1,17 +1,25 @@
 package lifegame;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-public class Timer {
+public class Timer implements Runnable {
     private List<Observer> observers;
+    private int delaySecond;
 
-    void setDelaySecond(int second) {
-
+    void setDelaySecond(int delaySecond) {
+        this.delaySecond = delaySecond;
     }
 
-    void run() {
-        for(Observer observer : observers) {
+    public void run() {
+        for (Observer observer : observers) {
             observer.tick();
+            try {
+                TimeUnit.SECONDS.sleep(delaySecond);
+            } catch (InterruptedException e) {
+                // TODO - 로그 출력방법
+                e.printStackTrace();
+            }
         }
     }
 }
