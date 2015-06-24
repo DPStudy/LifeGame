eb9440b5c6a7a40de7c11a75b21ddbf1b52b19f2
ahongdaong88 <jicholkim@zuminternet.com>
2015-06-25 00:11:29 +0900

    #1 옵저버 추가, 삭제 할 수 있도록 구현하였습니다.

--- src/main/java/lifegame/Timer.java
+++ src/main/java/lifegame/Timer.java
@@ -1,14 +1,23 @@
 package lifegame;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Timer implements Runnable {
-    private List<Observer> observers;
+    private List<Observer> observers = new ArrayList<Observer>();
     private int delaySecond;
 
-    void setDelaySecond(int delaySecond) {
+    public void setDelaySecond(int delaySecond) {
         this.delaySecond = delaySecond;
+    }
+
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
     }
 
     public void run() {
