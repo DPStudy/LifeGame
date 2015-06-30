6f3e24852eed2df426da4e06ed1c68761a8c2d93
Mastojun <mastojun@gmail.com>
2015-06-30 20:51:48 +0900

    add draw function

--- src/main/java/lifegame/Drawer.java
+++ src/main/java/lifegame/Drawer.java
@@ -2,5 +2,6 @@ package lifegame;
 
 public interface Drawer {
     void drawBoard(int width, int height);
-    void drawCell(int x, int y);
+    void drawAliveCell(int x, int y);
+    void drawEmptyCell(int x, int y);
 }
--- src/main/java/lifegame/impl/ConsoleDrawer.java
+++ src/main/java/lifegame/impl/ConsoleDrawer.java
@@ -1,13 +1,30 @@
 package lifegame.impl;
 
+import jcurses.system.CharColor;
+import jcurses.widgets.DefaultLayoutManager;
+import jcurses.widgets.Label;
+import jcurses.widgets.WidgetsConstants;
+import jcurses.widgets.Window;
 import lifegame.Drawer;
 
 public class ConsoleDrawer implements Drawer{
-    public void drawBoard(int width, int height) {
 
+    @Override
+    public void drawBoard(int width, int height) {
+        // Do nohting!
+        // Console에서는 board를 그릴 경우 cell을 표현할 공간이 적어짐 (...)
     }
 
-    public void drawCell(int x, int y) {
+    // java에서 console에 특정 위치를 지정하여 출력하는 표준화된 방법이 없어서 (...)
+    // drawAliveCell와 darwEmptyCell은 순서에 맞추어 출력이 된다고 가정을 합니다.
+    @Override
+    public void drawAliveCell(int x, int y) {
+        System.out.print('X');
+    }
 
+    @Override
+    public void drawEmptyCell(int x, int y) {
+        System.out.print('_');
     }
 }
+
