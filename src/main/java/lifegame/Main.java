182b401317b944c1e440bebdca65e8d5461d5b3b
Mastojun <mastojun@gmail.com>
2015-07-02 19:31:14 +0900

    Add timer thread to run program

--- src/main/java/lifegame/Main.java
+++ src/main/java/lifegame/Main.java
@@ -2,9 +2,17 @@ package lifegame;
 
 import lifegame.impl.NormalBoard;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class Main {
 
     public static void main(String[] args) {
         Board gameBoard = new NormalBoard(18, 18);
+        Timer timer = new Timer(1);
+        timer.registerObserver(gameBoard);
+
+        Executor executor = Executors.newSingleThreadExecutor();
+        executor.execute(timer);
     }
 }
